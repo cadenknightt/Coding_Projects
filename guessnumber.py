9f0ab1a740b673bbbf4731d2b4000de9965a1c58
cadenknightt <99857412+cadenknightt@users.noreply.github.com>
2025-12-24 19:58:54 -0700

    Enhance guessnumber.py with code comments
    Added comments to explain the code functionality.

--- guessnumber.py
+++ guessnumber.py
@@ -1,34 +1,43 @@
+# The import statement makes the random generator available
 import random
 
+# This function acts as a guess the number simulator named playgame
 def playgame():
-     print("Welcome to Guess the Number!")
+     print("\nWelcome to Guess the Number!")
      print("I am thinking of a number from 1 and 500.")
      print("Start guessing and I will say higher or lower.\n")
 
+     # "secret_num" defines what the computer chooses as its number
      secret_num = random.randint(1, 500)
+     # "attempts" is a placeholder for the starting number of guesses the user has
      attempts = 0
 
-     while True:
+     # This code creates a loop for the game until the number is guessed correctly
+     while True: 
           try:
                guess = int(input("Place your guess: "))
           except ValueError:
                print("Please enter valid number.")
                continue
 
+          # This code adds the number attempts the user has made each time the program is run.
           attempts += 1
 
+          # The if and else statements provide a message to the user based on the user's
+          # guess on the random number computer stores
           if guess < secret_num:
                print("Higher!\n")
           elif guess > secret_num:
                print("Lower!\n")
           else:
-               print(f"Good job, you guessed the number in {attempts} attempts!")
+               print(f"Good job! You guessed the number in {attempts} attempts!")
                break
 
+# this allows the user to play the game and when the number is guessed
+# the user can decide to play again or stop (which ends the game).
 while True:
      playgame()
-     playagain = input("\nWould you like to play again? (yes/y or no/n): ")
+     playagain = input("\nWould you like to play again? (y/n): ")
      if playagain != "yes" and playagain != "y":
           print("\nThanks for playing! See ya!")
           break
-
